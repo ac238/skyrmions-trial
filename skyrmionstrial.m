1aca699d40470de797392b3fed2f338ae041c826
Alexander Canright <alexandercanright@gmail.com>
2024-02-18 22:55:07 -0600

    Graphed initial skyrmion state according to QHMF 35

--- skyrmionstrial.m
+++ skyrmionstrial.m
@@ -2,16 +2,24 @@
 N=50; % num of pts in each direction
 
 % create bounds of graph
-xlow=0;
-ylow=0;
-xhigh=10;
-yhigh=10;
+xlow=-5;
+ylow=-5;
+xhigh=5;
+yhigh=5;
 [xx,yy]=meshgrid(linspace(xlow,xhigh,N),linspace(ylow,yhigh,N));
 
-% apply function of position
-uu=sin(xx);
-vv=5*cos(yy);
+% initialize skyrmion according to QHMF 35
+n=4;
+z_0=0;
+lambda=2;
+for i = 1:N
+    for j = 1:N
+        omega=((xx(i,j)+yy(i,j)*1i)/lambda)^n;
+        m1_init(i,j)=4*real(omega)/((abs(omega))^2+4);
+        m2_init(i,j)=4*imag(omega)/((abs(omega))^2+4);
+    end
+end
 
 % plot
-quiver(xx,yy,uu,vv)
-axis([0 10 0 10])
+quiver(xx,yy,m1_init,m2_init)
+axis([xlow xhigh ylow yhigh])
