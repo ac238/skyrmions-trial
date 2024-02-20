15eea03dc5b3dae460f9055bd41455ce6c7491fd
Alexander Canright <alexandercanright@gmail.com>
2024-02-20 16:25:29 -0600

    Added dynamics

--- skyrmionstrial.m
+++ skyrmionstrial.m
@@ -12,14 +12,38 @@ yhigh=5;
 n=4;
 z_0=0;
 lambda=2;
-for x_index = 1:N
-    for y_index = 1:N
-        omega=((xx(x_index,y_index)+yy(x_index,y_index)*1i)/lambda)^n;
-        m1_init(x_index,y_index)=4*real(omega)/((abs(omega))^2+4);
-        m2_init(x_index,y_index)=4*imag(omega)/((abs(omega))^2+4);
+for x_ind = 1:N
+    for y_ind = 1:N
+        omega=((xx(x_ind,y_ind)+yy(x_ind,y_ind)*1i)/lambda)^n;
+        m1_init(x_ind,y_ind)=4*real(omega)/((abs(omega))^2+4);
+        m2_init(x_ind,y_ind)=4*imag(omega)/((abs(omega))^2+4);
     end
 end
 
 % plot
 quiver(xx,yy,m1_init,m2_init)
 axis([xlow xhigh ylow yhigh])
+
+% dynamics
+t=0;
+dt=0.1;
+while t<10
+    for x_ind = 1:N
+        for y_ind = 1:N
+            % differential equations
+            m1(x_ind,y_ind) = m1_init(x_ind,y_ind) - m2_init(x_ind,y_ind)*dt;
+            m2(x_ind,y_ind) = m2_init(x_ind,y_ind) + m1_init(x_ind,y_ind)*dt;
+        end
+    end
+    
+    % plot
+    quiver(xx,yy,m1,m2)
+    drawnow
+    
+    % reset for new loop
+    m1_init=m1;
+    m2_init=m2;
+    t=t+dt;
+end
+
+
