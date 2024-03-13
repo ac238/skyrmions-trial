f671ad8a897703560d0eb5bc06340cff7d01dd78
Alexander Canright <alexandercanright@gmail.com>
2024-03-12 22:46:33 -0500

    Tracked center and radius of skyrmion

--- skyrmionstrial.m
+++ skyrmionstrial.m
@@ -37,8 +37,8 @@ end
 
 % dynamics
 landau = 1; % change to 0/1 to turn off/on effects
-zeeman = 1;
-coulomb = 1;
+zeeman = 0;
+coulomb = 0;
 electric = 1;
 
 t=0;
@@ -47,9 +47,9 @@ dt=0.001;
 t_ind=1;
 kappa = 100;     % change strengths of effects
 gmuB = 10;
-q = 1000;
+q = -1000;
 El_amp = 100;
-El_freq = 1;
+El_freq = 10;
 Q_top = -n;
 Q_top_list = []; % store values for final plot
 E_B_list = [];
@@ -104,45 +104,46 @@ while t<t_final
     triple = sum( m(:,:,mod(1:3,3)+1).*m_x(:,:,mod(2:4,3)+1).*m_y(:,:,mod(3:5,3)+1) - m(:,:,mod(1:3,3)+1).*m_y(:,:,mod(2:4,3)+1).*m_x(:,:,mod(3:5,3)+1) ,3);
     denom = 1 + sum(m.*m_x,3) + sum(m.*m_y,3) + sum(m_x.*m_y,3);
     rho = 4*atan2(triple,denom)/(4*pi*dx*dy);
+    rho_avg = (rho(:,:)+rho(mod(-1:N-2,N)+1,:)+rho(:,mod(-1:N-2,N)+1)+rho(mod(-1:N-2,N)+1,mod(-1:N-2,N)+1))/4;
 
     % Coulomb term
     if coulomb == 1
         m_k1=zeros(N,N,3);
-        m_x=(m(mod(1:N,N)+1,1:N,:)-m(mod(-1:N-2,N)+1,1:N,:))/(2*dx);
-        m_y=(m(1:N,mod(1:N,N)+1,:)-m(1:N,mod(-1:N-2,N)+1,:))/(2*dy);
+        m_x=(m(mod(1:N,N)+1,1:N,:)-m)/(dx);
+        m_y=(m(1:N,mod(1:N,N)+1,:)-m)/(dy);
         parfor i = 1:N
             for j = 1:N
-                m_k1 = m_k1 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho(i,j)*dx*dy;
+                m_k1 = m_k1 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho_avg(i,j)*dx*dy;
             end
         end
 
         m_k2=zeros(N,N,3);
         m_k2arg = m + dt/2*m_k1;
-        m_x=(m_k2arg(mod(1:N,N)+1,1:N,:)-m_k2arg(mod(-1:N-2,N)+1,1:N,:))/(2*dx);
-        m_y=(m_k2arg(1:N,mod(1:N,N)+1,:)-m_k2arg(1:N,mod(-1:N-2,N)+1,:))/(2*dy);
+        m_x=(m_k2arg(mod(1:N,N)+1,1:N,:)-m_k2arg)/(dx);
+        m_y=(m_k2arg(1:N,mod(1:N,N)+1,:)-m_k2arg)/(dy);
         parfor i = 1:N
             for j = 1:N
-                m_k2 = m_k2 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho(i,j)*dx*dy;
+                m_k2 = m_k2 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho_avg(i,j)*dx*dy;
             end
         end
 
         m_k3=zeros(N,N,3);
         m_k3arg = m + dt/2*m_k2;
-        m_x=(m_k3arg(mod(1:N,N)+1,1:N,:)-m_k3arg(mod(-1:N-2,N)+1,1:N,:))/(2*dx);
-        m_y=(m_k3arg(1:N,mod(1:N,N)+1,:)-m_k3arg(1:N,mod(-1:N-2,N)+1,:))/(2*dy);
+        m_x=(m_k3arg(mod(1:N,N)+1,1:N,:)-m_k3arg)/(dx);
+        m_y=(m_k3arg(1:N,mod(1:N,N)+1,:)-m_k3arg)/(dy);
         parfor i = 1:N
             for j = 1:N
-                m_k3 = m_k3 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho(i,j)*dx*dy;
+                m_k3 = m_k3 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho_avg(i,j)*dx*dy;
             end
         end
 
         m_k4=zeros(N,N,3);
         m_k4arg = m + dt*m_k3;
-        m_x=(m_k4arg(mod(1:N,N)+1,1:N,:)-m_k4arg(mod(-1:N-2,N)+1,1:N,:))/(2*dx);
-        m_y=(m_k4arg(1:N,mod(1:N,N)+1,:)-m_k4arg(1:N,mod(-1:N-2,N)+1,:))/(2*dy);
+        m_x=(m_k4arg(mod(1:N,N)+1,1:N,:)-m_k4arg)/(dx);
+        m_y=(m_k4arg(1:N,mod(1:N,N)+1,:)-m_k4arg)/(dy);
         parfor i = 1:N
             for j = 1:N
-                m_k4 = m_k4 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho(i,j)*dx*dy;
+                m_k4 = m_k4 + (dist_x(:,:,:,i,j).*m_y - dist_y(:,:,:,i,j).*m_x )*rho_avg(i,j)*dx*dy;
             end
         end
 
@@ -155,6 +156,11 @@ while t<t_final
     Q_top_init = Q_top;
     Q_top=sum(sum(rho))*dx*dy  % topological charge
     S_z=sum(sum(m(:,:,3)));           % total spin in z-direction
+
+    cent_of_mass_x = sum(sum(rho.*xx))/Q_top*dx*dy;
+    cent_of_mass_y = sum(sum(rho.*yy))/Q_top*dx*dy
+    variance = sum(sum(rho.*((xx-cent_of_mass_x).^2+(yy-cent_of_mass_y).^2)))/Q_top*dx*dy;
+    st_dev = sqrt(variance)
 
     m_dx=(m(mod(1:N,N)+1,:,:)-m(mod(-1:N-2,N)+1,:,:))/(2*dx); 
     m_dy=(m(:,mod(1:N,N)+1,:)-m(:,mod(-1:N-2,N)+1,:))/(2*dy);
@@ -176,7 +182,8 @@ while t<t_final
     quiver(xx,yy,m(:,:,1),m(:,:,2)) % full 2D vector field
     %quiver(xx(:,N/2),zeros(1,N),m(N/2,:,1),m(N/2,:,3)) % 1D slice
     hold on
-    contour(xx(1:N-1,1:N-1)+dx/2,yy(1:N-1,1:N-1)+dy/2,rho(1:N-1,1:N-1),10) % color plot
+    quiver(cent_of_mass_x,cent_of_mass_y,st_dev,0,'r')
+    contour(xx-dx/2,yy-dy/2,rho_avg,10) % color plot
     hold off
     axis([xlow xhigh ylow yhigh])
     drawnow
@@ -190,7 +197,7 @@ while t<t_final
 end
 
 % to plot one of the energies:
-% plot(1:length(E_B_list),E_B_list)
+% plot(1:length(Q_top_list),Q_top_list)
 
 % draw saved data with no lag, can copypaste to console to do again
 for i = 1:t_ind
